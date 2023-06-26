5c5078eba89e4476dfc251a34a3bb3d3a329ba17
Carrie Kraft <carriekarft@gmail.com>
2023-06-25 18:51:03 -0700

    adding component tags, fetch request for data related to specific dog, saving that data in state, and mapping over state variable to create individual walk components

--- src/components/DogWalks.js
+++ src/components/DogWalks.js
@@ -1,11 +1,34 @@
 import DogInfo from "./DogInfo";
+import EditWalk from "./EditWalk";
 import NewWalkForm from "./NewWalkForm";
 import Walk from "./Walk";
+import {useState, useEffect} from 'react'
+
+
 
 function DogWalks() {
+    const [walks, setWalks] = useState([])
+    const id = 1
+
+    useEffect(() => {
+        fetch(`http://localhost:9292/dogs/${id}/walks`)
+        .then(r => r.json())
+        .then(walkData => setWalks(walkData))
+    }, [])
+
+    console.log(walks)
+    const mappingWalks = walks.map(walk => {
+        return <Walk key={walk.id} walk={walk}/>
+    })
+
+
     return(
         <div>
             <DogInfo />
+            <div>{mappingWalks}</div>
+            <NewWalkForm />
+            {/* hide element unless icon in Walk component clicked */}
+            <EditWalk />
         </div>
     )
 }

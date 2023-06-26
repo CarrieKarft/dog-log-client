129071e6eb4897d8de791ec262fbb1882204a657
Carrie Kraft <carriekarft@gmail.com>
2023-06-25 18:54:47 -0700

    building out form component for creating a new instance of Walk class, and saaving it to db in dog-log-server

--- src/components/NewWalkForm.js
+++ src/components/NewWalkForm.js
@@ -2,9 +2,56 @@
 
 function NewWalkForm() {
     return(
-        <div></div>
+        <div>
+            <h2>Time For A Walk!</h2>
+            <form>
+                {/* maybe have this taken care of automatically in post requst? */}
+            <label>Who did you walk?&nbsp;
+                    <select>
+                        <option></option>
+                        <option></option>
+                        <option></option>
+                    </select>
+                </label>
+                <label>Where did you walk?&nbsp;
+                    <input type="text"></input>
+                </label>
+                <label>Did NAME do their buisness?&nbsp;
+                    <select>
+                        <option>YES</option>
+                        <option>NO</option>
+                    </select>
+                </label>
+                <label>How long was your walk in minutes?&nbsp;
+                    <input type="text"></input>
+                </label>
+                <label>How many miles did you walk?&nbsp;
+                    <input type="text"></input>
+                </label>
+                <label>Did NAME recieve any medication?&nbsp;
+                    <select>
+                        <option>N/A</option>
+                        <option>YES</option>
+                        <option>NO</option>
+                    </select>
+                </label>
+                <label>Any additional notes?&nbsp;
+                    <textarea type="text"></textarea>
+                </label>
+                <input type="submit"></input>
+    
+            </form>
+        </div>
     )
 }
+
+//   t.string "location"
+// t.boolean "used_bathroom"
+// t.integer "duration_in_minutes"
+// t.float "distance_in_miles"
+// t.boolean "medication_given"
+// t.string "notes"
+// t.integer "dog_id"
 
 
 export default NewWalkForm;

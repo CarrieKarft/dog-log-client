c66c15466df338b5d59496ef8ad6738c8ca58df5
Carrie Kraft <carriekarft@gmail.com>
2023-06-26 19:06:13 -0700

    adding conditional to display loading message if fetch statement hasn't returned data yet, destructruing dog specific object to map over walks associated, and pass dog specific info to components

--- src/components/DogWalks.js
+++ src/components/DogWalks.js
@@ -7,25 +7,32 @@ import {useState, useEffect} from 'react'
 
 
 function DogWalks() {
-    const [walks, setWalks] = useState([])
-    const id = 1
+    const [dogWalks, setDogWalks] = useState(null)
+    const id = 2
 
     useEffect(() => {
         fetch(`http://localhost:9292/dogs/${id}/walks`)
         .then(r => r.json())
-        .then(walkData => setWalks(walkData))
-    }, [])
+        .then(dogWalkData => setDogWalks(dogWalkData))
+   
+    }, [id])
 
-    console.log(walks)
+    if (!dogWalks) return <h2>Loading...</h2>
+
+    const {name, breed, age, walks} = dogWalks
+   
+    
     const mappingWalks = walks.map(walk => {
-        return <Walk key={walk.id} walk={walk}/>
+        return <Walk key={walk.id} walk={walk} />
     })
-
 
     return(
         <div>
-            <DogInfo />
-            <div>{mappingWalks}</div>
+            <DogInfo name={name} breed={breed} age={age}/>
+            <div>
+                <h2>{name}'s Walks</h2>
+                <div>{mappingWalks}</div>
+            </div>
             <NewWalkForm />
             {/* hide element unless icon in Walk component clicked */}
             <EditWalk />

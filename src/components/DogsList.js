ded62a4411f33bf335301e132ab70b5b71272736
Carrie Kraft <carriekarft@gmail.com>
2023-06-28 18:51:34 -0700

    creating function to filter through dogs array based on search bar component, and render dog components based on the filterd array returned

--- src/components/DogsList.js
+++ src/components/DogsList.js
@@ -6,18 +6,34 @@ import {useEffect, useState} from 'react'
 
 function DogsList() {
     const [dogs, setDogs] = useState([])
+    const [searchDog, setSearchDog] = useState("")
+    // console.log(searchDog)
+
     useEffect(() => {
         fetch("http://localhost:9292/dogs")
         .then(r => r.json())
         .then(dogsData => setDogs(dogsData))
     }, [])
 
-    const mappingDogs = dogs.map(d => {
+    // function onHandleSearchSubmit(e) {
+    //     e.preventDefault()
+        console.log(searchDog)
+        const searchDogFilter = dogs.filter(dog => {
+            if (dog.name.includes(searchDog)) {
+                return dog
+            }
+        })
+        console.log(searchDogFilter)
+
+    // }
+
+
+    const mappingDogs = searchDogFilter.map(d => {
         return <Dog dog={d} key={d.id}/>
     })
     return(
         <div>
-            <SearchDogs />
+            <SearchDogs searchDog={searchDog} setSearchDog={setSearchDog} />
             <div>{mappingDogs}</div>
             <NewDogForm dogs={dogs} setDogs={setDogs}/>
         </div>

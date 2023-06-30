8400aaeab77a36455e62ed50ee100ebdf3e1ca6f
Carrie Kraft <carriekarft@gmail.com>
2023-06-30 14:06:07 -0700

    refactoring to fetch walks as an association of dogs rather than seperatly

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import './App.css';
 import TitleBanner from './components/TitleBanner';
 import DogsList from './components/DogsList';
 import DogWalks from './components/DogWalks';
+import {useEffect, useState} from 'react'
 
 
 function App() {
+  const [dogs, setDogs] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/dogs")
+    .then(r => r.json())
+    .then(dogsData => setDogs(dogsData))
+  }, [])
+  console.log(dogs)
+
   return (
     <div className="App">
      <TitleBanner />
      <Switch>
-      <Route exact path='/'>
-        <DogsList />
+      <Route exact path='/dogs'>
+        <DogsList dogs={dogs} setDogs={setDogs}/>
       </Route>
-      <Route path='/dogs'>
-        <DogWalks />
+      <Route path='/dogs/:dog_id/walks'>
+        <DogWalks dogs={dogs}/>
       </Route>
      </Switch>
     </div>

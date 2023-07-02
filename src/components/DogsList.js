08a1d67048cfa7c82d58b002e133111d2ce494d5
Carrie Kraft <carriekarft@gmail.com>
2023-07-01 19:14:35 -0700

    removing fetch for dogs and dogs state to move up within component hierarchy

--- src/components/DogsList.js
+++ src/components/DogsList.js
@@ -5,18 +5,9 @@ import {useEffect, useState} from 'react'
 
 
 function DogsList({dogs, setDogs}) {
-    // const [dogs, setDogs] = useState([])
+
     const [searchDog, setSearchDog] = useState("")
-    // console.log(searchDog)
 
-    // useEffect(() => {
-    //     fetch("http://localhost:9292/dogs")
-    //     .then(r => r.json())
-    //     .then(dogsData => setDogs(dogsData))
-    // }, [])
-
-    // function onHandleSearchSubmit(e) {
-    //     e.preventDefault()
         console.log(searchDog)
         const searchDogFilter = dogs.filter(dog => {
             if (dog.name.includes(searchDog)) {
@@ -24,9 +15,6 @@ function DogsList({dogs, setDogs}) {
             }
         })
         console.log(searchDogFilter)
-
-    // }
-
 
     const mappingDogs = searchDogFilter.map(d => {
         return <Dog dog={d} key={d.id} id={d.id}/>

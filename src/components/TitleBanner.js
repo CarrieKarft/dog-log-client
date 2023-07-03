3f1f4158c23940a8dc041eded14cc2900bcbd367
Carrie Kraft <carriekarft@gmail.com>
2023-07-03 14:29:59 -0700

    importing banner image and updating image src tag so that ther is no longer image rendering issue

--- src/components/TitleBanner.js
+++ src/components/TitleBanner.js
@@ -1,9 +1,9 @@
-
+import dog_walk_image from '../dog_walk_image.avif'
 
 function TitleBanner() {
     return(
         <div className="banner">
-            <img src='park_picture_banner.avif' alt="A drawing of people in a park walking dogs and biking" className="bannerImage"></img>
+            <img type="image/avif" src={dog_walk_image} alt="A drawing of people in a park walking dogs and biking" className="bannerImage"></img>
             <div className="bannerText">
                 <h1>DOG LOG</h1>
                 <h2>AN APP FOR PROFESSIONAL DOG WALKERS</h2>

8662791454e322bffe226c2d070dabfe57c80bea
Carrie Kraft <carriekarft@gmail.com>
2023-07-03 14:26:27 -0700

    adding inline styling to some JSX elements

--- src/components/DogInfo.js
+++ src/components/DogInfo.js
@@ -3,11 +3,13 @@
 function DogInfo({name, breed, age}) {
     // console.log(dogWalks)
     return(
-        <div>
-            <h2>Dog Info</h2>
-            <p>{name}</p>
-            <p>{breed}</p>
-            <p>{age}</p>
+        <div style={{color: 'white', fontSize: 'larger', borderStyle: 'solid', borderColor: 'white', borderWidth: '2px', borderRadius: '25px', margin: '5%'}}>
+            <h2 >Dog Info</h2>
+            <div className="dogInfoDiv">
+                <h3>Name: {name}</h3>
+                <h3>Breed: {breed}</h3>
+                <h3>Age: {age}</h3>
+            </div>
         </div>
     )
 }

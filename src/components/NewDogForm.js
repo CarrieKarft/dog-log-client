bb8e15ce1fb3dc3a2bd011bf0790055216f0335f
Carrie Kraft <carriekarft@gmail.com>
2023-07-03 14:28:06 -0700

    adding inline styling to some JSX elements

--- src/components/NewDogForm.js
+++ src/components/NewDogForm.js
@@ -29,46 +29,50 @@ function NewDogForm({dogs, setDogs}) {
     }
 
     return(
-        <form onSubmit={e => handleSubmit(e)}>
-            <label>Dog Name
-                <input 
-                type="text"
-                value={dogName}
-                onChange={e => setDogName(e.target.value)}
+        <div className='newDogDiv'>
+            <h2>Add A New Dog</h2>
+            <form className="dogForm" onSubmit={e => handleSubmit(e)}>
+                <label>Dog Name
+                    <input 
+                    type="text"
+                    value={dogName}
+                    onChange={e => setDogName(e.target.value)}
+                    ></input>
+                </label>
+                <label>Dog Breed
+                    <input 
+                    type="text"
+                    value={dogBreed}
+                    onChange={e => setDogBreed(e.target.value)}
+                    ></input>
+                </label>
+                <label>Dog Age
+                    <input 
+                    type="text"
+                    value={dogAge}
+                    onChange={e => setDogAge(e.target.value)}
+                    ></input>
+                </label>
+                <label>Energy Level
+                    <select type="select" value={dogEnergyLevel} onChange={e => setDogEnergyLevel(e.target.value)}>
+                        <option value="low">low</option>
+                        <option value="medium">medium</option>
+                        <option value="high">high</option>
+                    </select>
+                </label>
+                <label>Medication Needed?
+                    <input 
+                    type="text"
+                    value={dogMedication}
+                    onChange={e => setDogMedication(e.target.value)}
+                    ></input>
+                </label>
+                <input
+                style={{margin: '1%', padding: '1%', height: '15%', width: '15%'}}
+                type='submit'
                 ></input>
-            </label>
-            <label>Dog Breed
-                <input 
-                type="text"
-                value={dogBreed}
-                onChange={e => setDogBreed(e.target.value)}
-                ></input>
-            </label>
-            <label>Dog Age
-                <input 
-                type="text"
-                value={dogAge}
-                onChange={e => setDogAge(e.target.value)}
-                ></input>
-            </label>
-            <label>Energy Level
-                <select type="select" value={dogEnergyLevel} onChange={e => setDogEnergyLevel(e.target.value)}>
-                    <option value="low">low</option>
-                    <option value="medium">medium</option>
-                    <option value="high">high</option>
-                </select>
-            </label>
-            <label>Medication Needed?
-                <input 
-                type="text"
-                value={dogMedication}
-                onChange={e => setDogMedication(e.target.value)}
-                ></input>
-            </label>
-            <input
-            type='submit'
-            ></input>
-        </form>
+            </form>
+        </div>
     )
 }
 

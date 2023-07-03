c533052588edada665c9f8c90286148634ccd47a
Carrie Kraft <carriekarft@gmail.com>
2023-07-03 14:32:11 -0700

    installing version 6 of react router so need to update Switch to Routes, and need to update syntax of Rout components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import {Switch, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import './App.css';
+import './index.css';
+import {dog_walk_image} from './dog_walk_image.avif'
 import TitleBanner from './components/TitleBanner';
 import DogsList from './components/DogsList';
 import DogWalks from './components/DogWalks';
@@ -21,14 +23,14 @@ function App() {
   return (
     <div className="App">
      <TitleBanner />
-     <Switch>
-      <Route exact path='/dogs'>
-        <DogsList dogs={dogs} setDogs={setDogs}/>
-      </Route>
-      <Route path='/dogs/:dog_id/walks'>
-        <DogWalks dogs={dogs} setDogs={setDogs}/>
-      </Route>
-     </Switch>
+     <Routes>
+      <Route path='/dogs' element={ <DogsList dogs={dogs} setDogs={setDogs}/>} />
+        {/* <DogsList dogs={dogs} setDogs={setDogs}/> */}
+      {/* </Route> */}
+      <Route path='/dogs/:dog_id/walks' element={<DogWalks dogs={dogs} setDogs={setDogs}/>} />
+        {/* <DogWalks dogs={dogs} setDogs={setDogs}/> */}
+      {/* </Route> */}
+     </Routes>
     </div>
   );
 }

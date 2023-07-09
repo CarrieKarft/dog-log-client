a53bf71deb34f8fd8111ebcfed39453b45346277
Carrie Kraft <carriekarft@gmail.com>
2023-07-09 14:16:13 -0700

    deleting comments for assesment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {Routes, Route} from "react-router-dom";
 import './App.css';
 import './index.css';
-import {dog_walk_image} from './dog_walk_image.avif'
 import TitleBanner from './components/TitleBanner';
 import DogsList from './components/DogsList';
 import DogWalks from './components/DogWalks';
@@ -16,20 +15,16 @@ function App() {
     .then(r => r.json())
     .then(dogsData => setDogs(dogsData))
   }, [])
-  console.log(dogs)
 
-  // if (!dogs) return <h2>Loading...</h2>
+  if (!dogs) return <h2>Loading...</h2>
+  console.log(dogs)
 
   return (
     <div className="App">
      <TitleBanner />
      <Routes>
       <Route path='/dogs' element={ <DogsList dogs={dogs} setDogs={setDogs}/>} />
-        {/* <DogsList dogs={dogs} setDogs={setDogs}/> */}
-      {/* </Route> */}
       <Route path='/dogs/:dog_id/walks' element={<DogWalks dogs={dogs} setDogs={setDogs}/>} />
-        {/* <DogWalks dogs={dogs} setDogs={setDogs}/> */}
-      {/* </Route> */}
      </Routes>
     </div>
   );
